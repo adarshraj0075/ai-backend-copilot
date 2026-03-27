da4ba5c0681f61a84db806e70332f629d3bbc7de
adarsh <adarshraj2512@gmail.com>
2026-03-27 07:08:47 +0530

    fix: await asycn calls in askAi controller to prevent promise error

--- src/controllers/ai.controller.js
+++ src/controllers/ai.controller.js
@@ -4,10 +4,10 @@ const {getMessages,saveMessages} =require("../services/memory.service");
 async function askAi(req,res,next){
     try {
         const {userId,question}=req.body;
-        const messages=getMessages(userId);
+        const messages=await getMessages(userId);
         messages.push({role:"user",content:question});
-        const answer=getAiResponse(messages);
-        messages.push({role:"assistent",constent:answer})
+        const answer=await getAiResponse(messages);
+        messages.push({role:"assistant",content:answer})
         await saveMessages(userId,messages)
         res.json(answer);
     } catch (error) {
